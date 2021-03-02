68122efeba70ff646b86d777de5ff4cd2e39dd58
Nazar <nazariykharchuk@gmail.com>
2021-03-02 14:39:50 +0200

    Adding comments

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,12 @@
 
 int main() {
 	setlocale(LC_ALL, "russian");
-	int number_of_files;
-	string* name_of_files = file_search(&number_of_files);
+	int number_of_files;		//The number of scv files in the directory
+	string* name_of_files = file_search(&number_of_files);		//Function to search for csv files in the directory
 
 	cout << number_of_files << endl;
 	for (int i = 0; i < number_of_files; i++)
 		cout << name_of_files[i] << endl;
 
-	delete_mass(name_of_files);
+	delete_mass(name_of_files);		//Dynamic array delete function
 }
